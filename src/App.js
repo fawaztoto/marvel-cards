654117c21dbc25a79ae5f43b024829977d16274c
Fawaz <fawaztotonchy2@gmail.com>
2022-12-13 14:27:32 +0800

    clone marvel character's card final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Main } from './components/Main'
+import './components/Main.css'
+import {Routes, Route} from 'react-router-dom'
+import { Marvel } from './components/Marvel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Routes>
+      <Route path='/' element={<Main/>} />
+      <Route path='/:id' element={<Marvel/>} />
+     </Routes>
     </div>
   );
 }
